b278566f513df5a62e4f86f964428a64f2679f85
João Fabris <jvfabris21@gmail.com>
2023-07-15 16:17:50 -0300

    feat: add RecommendedTrips

---
+++ src/app/components/RecommendedTrips.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { prisma } from "@/lib/prisma";
+import TripIem from "@/components/TripItem";
+
+async function getTrips() {
+    const trips = await prisma.trip.findMany({})
+
+    return trips;
+}
+
+const RecommendedTrips = async () => {
+    const data = await getTrips()
+    return (
+        <div className="container mx-auto p-5">
+            <div className="flex items-center">
+                <div className="w-full h-[1px] bg-grayLighter"></div>
+                <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">Destinos recomendados</h2>
+                <div className="w-full h-[1px] bg-grayLighter"></div>
+            </div>
+            <div className="flex flex-col items-center mt-5 gap-5">
+                {data.map((trips) => (
+                    <TripIem key={trips.id} trip={trips} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default RecommendedTrips;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import TripSearch from "./components/TripSearch";
 import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   )
 }
