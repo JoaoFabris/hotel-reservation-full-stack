90af8f3659bdd5f06368953f044a7a1526ae8f7b
João Fabris <jvfabris21@gmail.com>
2023-07-24 22:54:09 -0300

    feat: prepare for deploy

--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,36 +1,49 @@
 import { prisma } from "@/lib/prisma";
-import React from "react";
-import TripHeader from "../components/TripHeader";
-import TripReservation from "../components/TripReservation";
-import TripDescription from "../components/TripDescription";
-import TripHighlights from "../components/TripHighlights";
-import TripLocation from "../components/TripLocation";
 
+import React from "react";
+import TripHeader from "./components/TripHeader";
+import TripReservation from "./components/TripReservation";
+import TripDescription from "./components/TripDescription";
+import TripHighlights from "./components/TripHighlights";
+import TripLocation from "./components/TripLocation";
 
 const getTripDetails = async (tripId: string) => {
-    const trip = await prisma.trip.findUnique({
-        where: {
-            id: tripId,
-        },
-    });
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: tripId,
+    },
+  });
 
-    return trip;
+  return trip;
 };
 
 const TripDetails = async ({ params }: { params: { tripId: string } }) => {
-    const trip = await getTripDetails(params.tripId);
+  const trip = await getTripDetails(params.tripId);
 
-    if (!trip) return null;
+  if (!trip) return null;
 
-    return (
-        <div className="container mx-auto">
-            <TripHeader trip={trip} />
-            <TripReservation tripId={trip.id} pricePerDay={trip.pricePerDay as any} tripStartDate={trip.startDate} tripEndDate={trip.endDate} maxGuests={trip.maxGuests} />
-            <TripDescription description={trip.description} />
-            <TripHighlights highlights={trip.highlights} />
-            <TripLocation location={trip.location} locationDescription={trip.locationDescription} />
+  return (
+    <div className="container mx-auto lg:px-40 lg:pt-10">
+      <TripHeader trip={trip} />
+      <div className="flex flex-col lg:flex-row lg:mt-12 lg:gap-20">
+        <div className="lg:order-2">
+          <TripReservation
+            tripId={trip.id}
+            pricePerDay={trip.pricePerDay as any}
+            tripStartDate={trip.startDate}
+            tripEndDate={trip.endDate}
+            maxGuests={trip.maxGuests}
+          />
         </div>
-    );
+
+        <div className="lg:order-1">
+          <TripDescription description={trip.description} />
+          <TripHighlights highlights={trip.highlights} />
+        </div>
+      </div>
+      <TripLocation locationDescription={trip.locationDescription} location={trip.location} />
+    </div>
+  );
 };
 
 export default TripDetails;
