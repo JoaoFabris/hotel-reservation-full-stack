89c041b432723bd33c1e958b97cc804614c916fd
João Fabris <jvfabris21@gmail.com>
2023-07-25 02:11:57 -0300

    style: trip location for desktop

--- src/app/trips/[tripId]/components/TripLocation.tsx
+++ src/app/trips/[tripId]/components/TripLocation.tsx
@@ -9,16 +9,21 @@ interface TripLocationProps {
 
 const TripLocation = ({ location, locationDescription }: TripLocationProps) => {
     return (
-        <div className="p-5">
-            <h2 className="font-semibold text-primaryDarker mb-5">Localização</h2>
-            <div className="relative h-[280px] w-full">
+        <div className="p-5 lg:mt-12 lg:p-0 lg:pb-20">
+            <h2 className="font-semibold text-primaryDarker mb-5 lg:text-xl">Localização</h2>
+            <div className="relative h-[280px] w-full lg:hidden">
                 <Image className="rounded-lg shadow-md" src="/map-mobile.png" alt={location} fill style={{
                     objectFit: "cover"
                 }} />
             </div>
+            <div className="relative h-[480px] w-full hidden lg:block">
+                <Image className="rounded-lg shadow-md" src="/map-desktop.png" alt={location} fill style={{
+                    objectFit: "cover"
+                }} />
+            </div>
 
-            <p className="text-primaryDarker text-sm font-semibold mt-3">{location}</p>
-            <p className="text-xs text-primaryDarker mt-2 leading-5">{locationDescription}</p>
+            <p className="text-primaryDarker text-sm font-semibold mt-3 lg:text-base lg:mt-5">{location}</p>
+            <p className="text-xs text-primaryDarker mt-2 leading-5 lg:text-sm lg:mt-4">{locationDescription}</p>
             <Button variant="outlined" className="w-full mt-5">Ver no Google Maps</Button>
         </div>
     )
--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -11,7 +11,7 @@ import { useRouter } from "next/navigation";
 
 import Button from "@/components/Button";
 
-import {loadStripe} from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import { Trip } from "@prisma/client";
 import { toast } from "react-toastify";
 
@@ -57,19 +57,19 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
 
     const handleBuyClick = async () => {
         const res = await fetch("/api/payment", {
-          method: "POST",
-          body: Buffer.from(
-            JSON.stringify({
-              tripId: params.tripId,
-              startDate: searchParams.get("startDate"),
-              endDate: searchParams.get("endDate"),
-              guests: Number(searchParams.get("guests")),
-              totalPrice,
-              coverImage: trip.coverImage,
-              name: trip.name,
-              description: trip.description,
-            })
-          ),
+            method: "POST",
+            body: Buffer.from(
+                JSON.stringify({
+                    tripId: params.tripId,
+                    startDate: searchParams.get("startDate"),
+                    endDate: searchParams.get("endDate"),
+                    guests: Number(searchParams.get("guests")),
+                    totalPrice,
+                    coverImage: trip.coverImage,
+                    name: trip.name,
+                    description: trip.description,
+                })
+            ),
         });
 
         if (!res.ok) {
@@ -91,46 +91,49 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
     const guests = searchParams.get("guests")
 
     return (
-        <div className="container mx-auto p-5">
-            <h1 className="font-semibold text-xl text-primaryDarker"> Sua viagem </h1>
+        <div className="container mx-auto p-5 lg:max-w-[600px]">
+            <div className="container mx-auto p-5">
+                <h1 className="font-semibold text-xl text-primaryDarker"> Sua viagem </h1>
 
-            <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
-                <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
-                    <div className="relative h-[106px] w-[124px]">
-                        <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
-                    </div>
+                <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+                    <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
+                        <div className="relative h-[106px] w-[124px]">
+                            <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
+                        </div>
 
-                    <div className="flex flex-col">
-                        <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
-                        <div className="flex items-center gap-1">
-                            <ReactCountryFlag countryCode={trip.countryCode} svg />
-                            <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+                        <div className="flex flex-col">
+                            <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+                            <div className="flex items-center gap-1">
+                                <ReactCountryFlag countryCode={trip.countryCode} svg />
+                                <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+                            </div>
                         </div>
                     </div>
+
+                    <h3 className="font-semibold text-lg text-primaryDarker mt-3">Informações sobre o preço</h3>
+
+                    <div className="flex justify-between mt-1">
+                        <p className="text-primaryDarker">Total:</p>
+                        <p className="font-medium">R${totalPrice}</p>
+                    </div>
                 </div>
 
-                <h3 className="font-semibold text-lg text-primaryDarker mt-3">Informações sobre o preço</h3>
+                <div className="flex flex-col mt-5 text-primaryDarker">
+                    <h3 className="font-semibold ">Data:</h3>
+                    <div className="flex item-center gap-1 mt-1">
+                        <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+                        {" - "}
+                        <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
+                    </div>
+                    <h3 className="font-semibold mt-5">Hóspedes:</h3>
 
-                <div className="flex justify-between mt-1">
-                    <p className="text-primaryDarker">Total:</p>
-                    <p className="font-medium">R${totalPrice}</p>
+                    <p>{guests} hóspedes</p>
+                    <Button className="mt-5" onClick={handleBuyClick}>Finalizar compra</Button>
                 </div>
-            </div>
-
-            <div className="flex flex-col mt-5 text-primaryDarker">
-                <h3 className="font-semibold ">Data:</h3>
-                <div className="flex item-center gap-1 mt-1">
-                    <p>{format(startDate, "dd 'de' MMMM", { locale: ptBR })}</p>
-                    {" - "}
-                    <p>{format(endDate, "dd 'de' MMMM", { locale: ptBR })}</p>
-                </div>
-                <h3 className="font-semibold mt-5">Hóspedes:</h3>
-
-                <p>{guests} hóspedes</p>
-                <Button className="mt-5" onClick={handleBuyClick}>Finalizar compra</Button>
             </div>
         </div>
     )
+
 }
 
 export default TripConfirmation;
